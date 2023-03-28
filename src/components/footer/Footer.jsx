66439e4b41bf9d464f2deadea09301465cde0f51
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-03-28 20:34:47 +0800

    height

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+import SendIcon from '@mui/icons-material/Send';
 
 const Footer = () => {
   return (
-    <div className='footer'>created by: Bruce Andrada &copy;</div>
+    <div className='footer'><div className='footer__text'>created by: Bruce Andrada &copy;</div>
+    <div className='footer__contact'>    <a href="https://www.linkedin.com/in/bruce-michael-andrada-565b561a4/"><LinkedInIcon  className='icon'/></a>
+    <a href="https://bruce-folio.vercel.app/"><ContactPageIcon  className='icon'/></a>
+    <a href = "mailto: bmpandrada@gmail.com"><SendIcon  className='icon'/></a></div></div>
   )
 }
 
