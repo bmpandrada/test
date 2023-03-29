33209ef2bc4ca1a6263f67c9b85242dd8c0b519d
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-03-29 13:07:34 +0800

    all

--- src/components/navbar/Nabar.jsx
+++ src/components/navbar/Nabar.jsx
@@ -37,14 +37,14 @@ const Nabar = () => {
             {submenu.map((subItem, subIndex)=>{
               return(<li className='navbar__sub-item' key={subIndex}
               ><DoubleArrowIcon className='navbar__iconSm'/>
-                <Link to={`${subItem.links}`} onClick={()=>setToggle(!toogle)}>{subItem.titleSub}</Link>
+                <Link  onClick={()=>setToggle(!toogle)}>{subItem.titleSub}</Link>
               </li>)
             })}
           </ul>
         )}
       </li>
     })}
-      
+      {/* to={`${subItem.links} `} */}
     </ul>
     <div className='navbar__burger' onClick={()=>setToggle(!toogle)}>
     {toogle ?  <MenuIcon style={{ fontSize: "3rem",  transition: 'all 0.6s ease-in' }} /> : 
